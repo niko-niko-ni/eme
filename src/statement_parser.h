69867652f828c1bad31adb4d827f202a74c4d3dd
Niko <the.nikonikoni@gmail.com>
2020-08-11 10:41:49 -0500

    Added extra comments to make the behavior of the statement parser functions clearer.

--- src/statement_parser.h
+++ src/statement_parser.h
@@ -3,8 +3,12 @@
 #include "token.h"
 
 
-// Returns success or failure
+// Returns true for success, false for failure
 // The resulting parsed statement is placed in 'result', and 'remaining_tokens_list''s first element is updated to contain the element after this statement's semicolon.
 bool parse_statement(Token *result, Token_Linked_List *remaining_tokens_list);
 
+
+// Parses a list of semicolon-separated statements into a list of statement tokens.
+// Returns true for success, false for failure.
+// The resulting list of statement tokens is placed in 'resulting_list'.
 bool parse_statements(Token_Linked_List *resulting_list, Token_Linked_List tokens);
