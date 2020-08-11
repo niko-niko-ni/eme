9de78d19b3f0f02b76e735196fb1b88ffa7f9b66
Niko <the.nikonikoni@gmail.com>
2020-08-10 21:09:34 -0500

    Stopped defining uint type on everything except MSVC in order to remove naming conflict with predefined uint type in gcc

--- src/types.h
+++ src/types.h
@@ -6,7 +6,10 @@ typedef uint8_t  u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
+
+#if defined (_MSC_VER)
 typedef uint_fast32_t uint;
+#endif
 
 typedef int8_t   i8;
 typedef int16_t  i16;
