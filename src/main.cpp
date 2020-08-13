0276ba31a8a2ece0d52449979fcc46e61d837274
Arjun Khanna <arjunk2022@headroyce.org>
2020-08-12 20:05:44 -0700

    added parentheses functionality

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,15 @@
 
 int main() {
   try {
+    printf("main block started. moving on to lexing...\n");
     Token_Linked_List tokens = lex_file((char*)"./examples/basicstatements.eme");
-
+    printf("lexer finished. moving on to declaring list of statements...\n");
     Token_Linked_List statements;
+    printf("list of statements declared. Moving on to parsing statements...\n");
     parse_statements(&statements, tokens);
-
+    printf("statements parsed. moving onto printing all tokens...\n");
     print_all_tokens_after(*statements.first, 0);
-
+    printf("all tokens printed \n");
 
   } catch(const std::exception& e) {
     printf("Error: %s\n", e.what());
--- src/statement_parser.cpp
+++ src/statement_parser.cpp
@@ -10,6 +10,7 @@ bool parse_one_statement(Token *result, Token_Linked_List *remaining_tokens_list
   Token *current_token = remaining_tokens_list->first;
 
   while(current_token->type != token_eol) {
+    printf("starting iteration...\n");
     if(current_token->type == token_syntax && current_token->data.syntax == ';') {
       Token_Linked_List sub_tokens;
       sub_tokens.first = remaining_tokens_list->first;
@@ -28,39 +29,52 @@ bool parse_one_statement(Token *result, Token_Linked_List *remaining_tokens_list
 
 
       result->data.sub_tokens = sub_tokens;
-
+      printf("no parentheses. returning to parse_statements\n");
       return true;
     } else if (current_token->type == token_syntax && (current_token->data.syntax == '(' || (current_token->data.syntax == '{' || current_token->data.syntax == '['))) {
+      printf("parentheses found. parsing parentheses...\n");
       int nextNum = parse_parentheses(current_token);
+      printf("parentheses parsed\n");
       Token *replace_token = current_token;
-      for(int i = 0; i <= nextNum; i++) {
+      std::cout << nextNum << " tokens to skip. skipping tokens..." << '\n';
+      for(int i = 0; i < nextNum; i++) {
         replace_token = replace_token->next;
       }
+      printf("next token found. deleting tokens...\n");
       current_token->next = replace_token;
+      printf("tokens deleted. continuing loop...\n");
     }
 
     // @Incomplete: deal with if an end curly brace, bracket, or parentheses is encountered outside of its context
-
     current_token = current_token->next;
+    printf("iteration complete.\n");
   }
-
+  printf("loop complete. returning to parse_statements...\n");
   return false;
 }
 
 int parse_parentheses(Token *token) {
+    printf("Starting parentheses parsing function. defining variables...\n");
     Token *current_token = token->next;
-    uint ret = 1;
+    int ret = 1;
     Token_Linked_List sub_tokens;
+    printf("Variables defined. modifying token...\n");
     token->type = token_expr_parentheses; 
     token->data.sub_tokens = sub_tokens;
-    Token* eol;
+    printf("token modified. defining eol...\n");
+    Token *eol = new Token();
+    printf("eol defines. modifying eol...\n");
     eol->character = -1;
+    printf("eol character modified. modifying line...\n");
     eol->line = -1;
+    printf("eol line modified. modifying type...\n");
     eol->type = token_eol;
+    printf("eol type modified. iterating subtokens...\n");
     sub_tokens.first = eol;
     bool first = true;
-    while (current_token->data.syntax != token->data.syntax && current_token->type != token_eol) {
-      if (current_token->type == token_syntax && (current_token->data.syntax == '(' || (current_token->data.syntax == '{' || current_token->data.syntax == '['))) {
+
+    while (current_token->type != token_eol) {
+      if (current_token->type == token_syntax && ((current_token->data.syntax == ')' && token->data.syntax == '(') || ((current_token->data.syntax == '}' &&token->data.syntax == '{') || (current_token->data.syntax == ']' && token->data.syntax == '[')))) {
         ret = ret + parse_parentheses(current_token);
       }
       if (first) {
@@ -71,17 +85,22 @@ int parse_parentheses(Token *token) {
 
       ret++;
       current_token  = current_token->next;
+      printf("iteration complete. modifying token and sub_tokens...\n");
     }
     token->data.sub_tokens = sub_tokens;
     sub_tokens.last = eol;
+    return ret;
 }
 
 bool parse_statements(Token_Linked_List *resulting_list, Token_Linked_List tokens) {
+  printf("parsing all statements, iterating through statements...\n");
   bool resulting_list_initialized = false;
   Token_Linked_List parsed = tokens;
   while(parsed.first->type != token_eol) {
     Token *result = new Token();
+    printf("sending statement to individual statement parser...\n");
     bool succeeded = parse_one_statement(result, &parsed);
+    printf("done parsing individual statement\n");
 
     if(succeeded) {
       if(resulting_list_initialized) {
@@ -96,6 +115,7 @@ bool parse_statements(Token_Linked_List *resulting_list, Token_Linked_List token
       return false; // @Incomplete: maybe put an error message here?
     }
   }
+  printf("finished iterating through statements. returning to main...\n");
   return true;
 }
 /*
